04ce19b47c5a810451e585cb31357fd79b31209d
ketanmodi087 <ketan.kanhasoft@gmail.com>
2026-08-14 11:10:44 +0530

    feat: add documents changes

---
+++ app/Http/Requests/DocumentRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Helpers\Helper;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class DocumentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'name' => 'required',
+        ];
+
+        return $rules;
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        Helper::sendError('validation error', $validator->errors());
+    }
+}
